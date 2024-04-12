18e62c7217b05bdab461f0f14ac6ba52fe8ffc07
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2024-04-12 09:55:30 -0300

    feat: add react-toastify

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
+import "react-toastify/dist/ReactToastify.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
+      <ToastContainer />
     </ChakraProvider>
   </React.StrictMode>
 );
